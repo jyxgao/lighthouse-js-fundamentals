6bbb5a5bf5a0536e8a35c394e0de70f3cd14e1ad
Jenny Gao <gao.jenny9@gmail.com>
2020-06-04 22:09:49 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,19 @@
+// Example 1
+const isCitizen = true;
+const age = 24;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+// Example 2
+const temperature = -45;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// Example 3
 raining = false;
 
 if (!raining) {
